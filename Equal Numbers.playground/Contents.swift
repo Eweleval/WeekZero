76b0921c80f754f6e047043c0d991bf43b49b739
Wellz <ewele.val-okenyi@decagon.dev>
2022-02-09 09:29:04 +0100

    Make My Code Shorter

--- Equal Numbers.playground/Contents.swift
+++ Equal Numbers.playground/Contents.swift
@@ -1,12 +1,7 @@
 import UIKit
 
 func isSameNum(_ num1: Int, _ num2: Int) -> Bool{
-    if num1 == num2{
-        return true;
-    }
-    else{
-        return false;
-    }
+    return num1 == num2 ? true : false;
 }
 isSameNum(4,8);
 isSameNum(2,2);
